81320e09ba6592885f33ab5c3d74e96e9d415ecc
hriteshseereon <seereon.hritesh@gmail.com>
2026-03-09 17:24:23 +0530

    update validation in the broker tab

--- adminfrontend/src/modules/DMS/pages/module/Master/Business/tabs/BrokerTab.jsx
+++ adminfrontend/src/modules/DMS/pages/module/Master/Business/tabs/BrokerTab.jsx
@@ -598,7 +598,16 @@ export default function BrokerTab() {
                 </Form.Item>
               </Col>
               <Col span={4}>
-                <Form.Item label="City" name="permanent_city">
+                <Form.Item
+                  label="City"
+                  name="permanent_city"
+                  rules={[
+                    {
+                      pattern: /^[a-zA-Z\s]+$/,
+                      message: "Only letters and spaces are allowed",
+                    },
+                  ]}
+                >
                   <Input
                     className={inputClass}
                     disabled={viewMode}
@@ -607,7 +616,16 @@ export default function BrokerTab() {
                 </Form.Item>
               </Col>
               <Col span={4}>
-                <Form.Item label="District" name="permanent_district">
+                <Form.Item
+                  label="District"
+                  name="permanent_district"
+                  rules={[
+                    {
+                      pattern: /^[a-zA-Z\s]+$/,
+                      message: "Only letters and spaces are allowed",
+                    },
+                  ]}
+                >
                   <Input
                     className={inputClass}
                     disabled={viewMode}
@@ -616,7 +634,16 @@ export default function BrokerTab() {
                 </Form.Item>
               </Col>
               <Col span={4}>
-                <Form.Item label="State" name="permanent_state">
+                <Form.Item
+                  label="State"
+                  name="permanent_state"
+                  rules={[
+                    {
+                      pattern: /^[a-zA-Z\s]+$/,
+                      message: "Only letters and spaces are allowed",
+                    },
+                  ]}
+                >
                   <Input
                     className={inputClass}
                     disabled={viewMode}
@@ -625,7 +652,16 @@ export default function BrokerTab() {
                 </Form.Item>
               </Col>
               <Col span={4}>
-                <Form.Item label="Pin Code" name="permanent_pin">
+                <Form.Item
+                  label="Pin Code"
+                  name="permanent_pin"
+                  rules={[
+                    {
+                      pattern: /^[0-9]{6}$/,
+                      message: "Only numbers are allowed",
+                    },
+                  ]}
+                >
                   <Input
                     className={inputClass}
                     disabled={viewMode}
@@ -727,7 +763,16 @@ export default function BrokerTab() {
 
               {/* Aadhar */}
               <Col span={4}>
-                <Form.Item label="Aadhar Number" name="aadharNo">
+                <Form.Item
+                  label="Aadhar Number"
+                  name="aadharNo"
+                  rules={[
+                    {
+                      pattern: /^[0-9]{12}$/,
+                      message: "Enter a valid 12-digit Aadhaar number",
+                    },
+                  ]}
+                >
                   <Input
                     className={inputClass}
                     disabled={viewMode}
