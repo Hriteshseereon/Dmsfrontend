7ad3cb9a76d47a38a710bdb28555cf77d7ddf748
hriteshseereon <seereon.hritesh@gmail.com>
2026-05-20 15:54:33 +0530

    added the cityjson data

--- adminfrontend/src/modules/DMS/pages/module/Master/Business/tabs/cityData.js
+++ adminfrontend/src/modules/DMS/pages/module/Master/Business/tabs/cityData.js
@@ -1,83 +1,81 @@
 export const cityData = {
   Odisha: {
-    Khordha: ["Bhubaneswar", "Khordha", "Jatani", "Banapur", "Tangi"],
-    Cuttack: ["Cuttack", "Choudwar", "Athagarh", "Banki"],
-    Puri: ["Puri", "Pipili", "Konark"],
-    Balasore: ["Balasore", "Soro", "Jaleswar", "Nilagiri"],
-    Bhadrak: ["Bhadrak", "Basudevpur", "Dhamnagar"],
-    Jajapur: ["Jajpur", "Vyasanagar", "Panikoili"],
-    Jagatsinghpur: ["Jagatsinghpur", "Paradip", "Biridi"],
-    Kendrapara: ["Kendrapara", "Pattamundai", "Marshaghai"],
-    Dhenkanal: ["Dhenkanal", "Bhuban", "Kamakshyanagar"],
-    Angul: ["Angul", "Talcher", "Athmallik"],
-
-    Sambalpur: ["Sambalpur", "Rengali", "Hirakud"],
-    Sundargarh: ["Rourkela", "Rajgangpur", "Sundargarh", "Biramitrapur"],
-    Jharsuguda: ["Jharsuguda", "Brajrajnagar", "Belpahar"],
-    Bargarh: ["Bargarh", "Padampur", "Attabira", "Barpali"],
-    Balangir: ["Balangir", "Titlagarh", "Kantabanji", "Patnagarh"],
-    Subarnapur: ["Sonepur", "Tarbha", "Binika"],
-    Nuapada: ["Nuapada", "Khariar", "Khariar Road"],
-    Kalahandi: ["Bhawanipatna", "Junagarh", "Kesinga", "Dharamgarh"],
-
-    Ganjam: ["Brahmapur", "Hinjilikat", "Aska", "Chatrapur", "Polsara", "Digapahandi"],
-    Gajapati: ["Parlakhemundi", "Kashinagar"],
-    Rayagada: ["Rayagada", "Gunupur", "Bissam Cuttack"],
-    Koraput: ["Jeypore", "Koraput", "Sunabeda", "Kotpad", "Borigumma"],
-    Malkangiri: ["Malkangiri", "Balimela", "Kalimela"],
-    Nabarangpur: ["Nabarangpur", "Umerkote", "Papadahandi"],
-    Kandhamal: ["Phulbani", "G. Udayagiri", "Baliguda"],
-    Boudh: ["Boudh"],
-    Nayagarh: ["Nayagarh", "Daspalla", "Khandapada"],
-
-    Mayurbhanj: ["Baripada", "Rairangpur", "Karanjia"],
-    Keonjhar: ["Keonjhar", "Barbil", "Joda", "Anandapur"],
-    Deogarh: ["Deogarh"],
+    Khordha: ["Balianta","Balipatna","Banapur","Begunia","Bhubaneswar","Bolagarh","Chilika","Jatni","Khordha","Tangi"],
+    Cuttack: ["Athagad","Badamba","Banki","Banki- Dampara","Baranga","Cuttacksadar","Kantapada","Mahanga","Narasinghpur","Niali","Nischinta Koili","Salepur","Tangi Choudwar","Tigiria"],
+    Puri: ["Puri", "Pipili", "Konark","Astaranga","Brahmagiri","Delanga","Gop","Kakat Pur","Kanas","Krushnaprasad","Nimapada","Sadar","Satyabadi"],
+    Balasore: ["Bahanaga","Baleshwar","Baliapal","Basta","Bhograi","Jaleswar","Khaira","Nilgiri","Oupada","Remuna","Simulia","Soro"],
+    Bhadrak: ["Basudevpur","Bhadrak","Bhandaripokhari","Bonth","Chandabali","Dhamanagar","Tihidi"],
+    Jajapur: ["Jajpur", "Vyasanagar", "Panikoili","Badchana","Bari","Binjharpur","Dahrmasala","Danagadi","Dasarathapur","Korei","Rasulpur","Sukinda"],
+    Jagatsinghpur: ["Balikuda","Biridi","Erasama","Jagatsinghpur","Kujang","Naugaon","Raghunathpur","Tirtol","Paradip"],
+    Kendrapara: ["Kendrapara", "Pattamundai", "Marshaghai","Aul","Derabish","Garadapur","Kendrapada","Mahakalapada","Marsaghai","Rajkanika","Rajnagar"],
+    Dhenkanal: ["Bhuban","Dhenkanal Sadar","Gondia","Hindol","Kamakhyanagar","Kankada Had","Odapada","Parjang"],
+    Angul: ["Angul", "Talcher", "Athmallik","Banarpal","Chhendipada","Kaniha","Kishorenagar","Palalahada"],
+ 
+    Sambalpur: ["Sambalpur", "Rengali", "Hirakud","Bamra","Dhankauda","Jamankira","Jujomura","Kuchinda","Maneswar","Naktideul","Rairakhol",],
+    Sundargarh: ["Rourkela", "Rajgangpur", "Sundargarh", "Biramitrapur","Balisankara","Bargaon","Bisra","Bonaigarh","Gurundia","Hemgir","Koida","Kuarmunda","Kutra","Lahunipara","Lathikata","Lephripara","Nuagaon","Subdega","Tangarpali"],
+    Jharsuguda: ["Jharsuguda", "Brajrajnagar", "Belpahar","Kirmira","Kolabira","Laikera","Lakhanpur"],
+    Bargarh: ["Ambabhona","Attabira","Bargarh","Barpali","Bhatli","Bheden","Bijepur","Gaisilet","Jharbandh","Padampur","Paikmal","Sohella"],
+    Balangir: ["Agalpur","Balangir","Bangomunda","Belpara","Deogaon","Gudvella","Khaprakhol","Loisinga","Muribahal","Patnagarh","Puintala","Saintala","Titlagarh","Turekela","Kantabanji"],
+    Subarnapur: ["Binika","Birmaharajpur","Dunguripali","Sonepur","Tarbha","Ullunda"],
+    Nuapada: ["Boden","Khariar","Komna","Nuapada","Sinapali"],
+    Kalahandi: ["Bhawanipatna","Dharamagarh","Golamunda","Jayapatna","Junagarh","Kalampur","Karlamunda","Kesinga","Kokasara","Lanjigarh","Madanpur Rampur","Narala","Thuamul Ram Pur", "Dharamgarh"],
+ 
+    Ganjam: ["Brahmapur", "Aska","Beguniapada","Bellaguntha","Bhanjanagar","Buguda","Chatrapur","Chikiti","Dharakote","Digapahandi","Ganjam","Hinjilicut","Jagannathprasad","Kabisuryanagar","Khallikote","Kukudakhandi","Patrapur","Polosara","Purushottampur","Rangeilunda","Sanakhemundi","Sheragada","Surada"],
+    Gajapati: ["Parlakhemundi", "Kashinagar","Gosani","Gumma","Kasinagar","Mohana","Nuagada","R.Udayagiri","Rayagada"],
+    Rayagada: ["Bissamcuttack","Chandrapur","Gudari","Gunupur","Kalyansingpur","Kasipur","Kolnara","Muniguda","Padmapur","Ramanaguda","Rayagada"],
+    Koraput: ["Bandhugaon","Boipariguda","Borigumma","Dasamantapur","Jeypore","Koraput","Kotpad","Kundura","Lamtaput","Laxmipur","Nandapur","Narayan Patana","Pottangi","Semiliguda","Sunabeda"],
+    Malkangiri: ["Chitrakonda","Kalimela","Khairaput","Korukonda","Malkangiri","Mathili","Podia","Balimela"],
+    Nabarangpur: ["Chandahandi","Dabugam","Jharigam","Kosagumuda","Nabarangpur","Nandahandi","Papadahandi","Raighar","Tentulikhunti","Umerkote"],
+    Kandhamal: ["Baliguda","Chakapad","Daringibadi","G.Udayagiri","K.Nuagan","Khajuripada","Kotagarh","Phiringia","Phulbani","Raikia","Tikabali","Tumudibandh"],
+    Boudh: ["Boudh","Harabhanga","Kantamal"],
+    Nayagarh: ["Nayagarh", "Daspalla", "Khandapada","Bhapur", "Gania","Khandapara","Nayagarh","Nuagaon","Odagaon","Ranapur"],
+ 
+    Mayurbhanj: ["Badasahi","Bahalda","Bangriposi","Baripada","Betnoti","Bijatala","Bisoi","Gopabandhunagar","Jamda","Joshipur","Kaptipada","Karanjia","Khunta","Kuliana","Kusumi","Morada","Rairangpur","Raruan","Rasgovindpur","Samakhunta","Saraskana","Sukruli","Suliapada","Thakurmunda","Tiring","Udala"],
+    Keonjhar: ["Anandapur","Bansapal","Champua","Ghasipura","Ghatgaon","Harichadanpur","Hatadihi","Jhumpura","Joda","Kendujhar Sadar","Patana","Saharapada","Telkoi","Barbil"],
+    Deogarh: ["Deogarh","Barkote","Reamal","Tileibani"],
   },
-
+ 
   "West Bengal": {
     Kolkata: ["Kolkata"],
-
+ 
     "North 24 Parganas": [
-      "Barasat", "Bidhannagar", "Rajarhat", "Barrackpore", "Dum Dum",
+      "Bidhannagar", "Rajarhat", "Barrackpore", "Dum Dum",
       "Habra", "Naihati", "Titagarh", "Kamarhati", "Panihati",
-      "Bhatpara", "Bangaon", "Basirhat", "Madhyamgram"
+      "Bhatpara", "Bangaon", "Basirhat", "Madhyamgram","Amdanga", "Baduria", "Bagda", "Barasat-I", "Barasat-II", "Barrackpur-I", "Barrackpur-II", "Basirhat-I", "Basirhat-II", "Bongaon", "Deganga", "Gaighata", "Habra-I", "Habra-II", "Haroa", "Hasnabad", "Hingalganj", "Minakhan", "Rajarhat", "Sandeshkhali-I", "Sandeshkhali-II", "Swarupnagar"
     ],
-
+ 
     "South 24 Parganas": [
-      "Baruipur", "Sonarpur", "Maheshtala", "Budge Budge",
-      "Diamond Harbour", "Canning", "Jaynagar", "Rajpur"
+     "Baruipur", "Basanti", "Bhangar-I", "Bhangar-II", "Bishnupur-I", "Bishnupur-II", "Budge Budge-I", "Budge Budge-II", "Canning-I", "Canning-II", "Diamond Harbour-I", "Diamond Harbour-II", "Falta", "Gosaba", "Jaynagar-I", "Jaynagar-II", "Kakdwip", "Kulpi", "Kultali", "Magrahat-I", "Magrahat-II", "Mandirbazar", "Mathurapur-I", "Mathurapur-II", "Namkhana", "Pathar Pratima", "Sagar", "Sonarpur", "Thakurpukur Mahestola", "Maheshtala", "Rajpur"," Sonarpur"
+ 
     ],
-
-    Howrah: ["Howrah", "Uluberia", "Bally", "Domjur"],
-
-    Hooghly: [
-      "Chinsurah", "Chandannagar", "Serampore", "Rishra",
-      "Konnagar", "Uttarpara", "Dankuni", "Arambag", "Tarakeswar"
-    ],
-
-    "Paschim Bardhaman": ["Asansol", "Durgapur", "Raniganj", "Jamuria", "Kulti"],
-    "Purba Bardhaman": ["Bardhaman", "Kalna", "Katwa", "Memari", "Guskara"],
-
-    "Purba Medinipur": ["Tamluk", "Haldia", "Contai", "Egra", "Digha"],
-    "Paschim Medinipur": ["Midnapore", "Kharagpur", "Ghatal", "Garhbeta"],
-
-    Jhargram: ["Jhargram", "Belpahari", "Gopiballavpur", "Nayagram"],
-    Bankura: ["Bankura", "Bishnupur", "Sonamukhi"],
-    Purulia: ["Purulia", "Jhalda", "Raghunathpur"],
-
-    Birbhum: ["Suri", "Bolpur", "Rampurhat"],
-    Murshidabad: ["Baharampur", "Kandi", "Jangipur"],
-    Nadia: ["Krishnanagar", "Kalyani", "Nabadwip", "Ranaghat"],
-
-    Malda: ["Malda", "Old Malda", "Gazole"],
-    "Uttar Dinajpur": ["Raiganj", "Islampur", "Dalkhola"],
-    "Dakshin Dinajpur": ["Balurghat", "Gangarampur"],
-
-    Darjeeling: ["Darjeeling", "Siliguri", "Kurseong", "Mirik"],
-    Jalpaiguri: ["Jalpaiguri", "Malbazar", "Dhupguri"],
-    Alipurduar: ["Alipurduar", "Jaigaon", "Hasimara"],
-    "Cooch Behar": ["Cooch Behar", "Dinhata", "Mathabhanga"],
-    Kalimpong: ["Kalimpong", "Lava", "Pedong"],
+ 
+    Howrah: ["Howrah", "Amta-I","Amta-Ii","Bagnan-I","Bagnan-Ii","Bally-Jagacha","Jagatballavpur","Panchla","Sankrail","Shyampur-I","Shyampur-Ii","Udaynarayanpur","Uluberia-I","Uluberia-Ii"],
+ 
+    Hooghly: ["Arambagh","Balagarh","Chanditala-I","Chanditala-Ii","Chinsurah-Magrah","Dhaniakhali","Goghat-I","Goghat-Ii","Haripal","Jangipara","Khanakul-I","Khanakul-Ii","Pandua","Polba-Dadpur","Pursurah","Singur","Sirampur-Uttarpara"],
+ 
+    "Paschim Bardhaman": ["Asansol",  "Raniganj", "Jamuria", "Kulti","Barabani", "Faridpur-Durgapur", "Kanksa", "Ondal", "Pandabeswar", "Salanpur" ],
+    "Purba Bardhaman": ["Bardhaman", "Ausgram-I", "Ausgram-II", "Bhatar", "Burdwan-I", "Burdwan-II", "Galsi-I", "Galsi-II", "Jamalpur", "Kalna-II", "Kalna-I", "Katwa-I", "Katwa-II", "Ketugram-II", "Ketugram-I", "Khandaghosh", "Mangolkote", "Manteswar", "Memari-I", "Memari-II", "Purbasthali-I", "Purbasthali-II", "Raina-I", "Raina-II","Guskara"],
+ 
+    "Purba Medinipur": ["Bhagawanpur-I", "Bhagawanpur-II", "Chandipur", "Contai-I", "Contai-III", "Deshapran", "Egra-I", "Egra-II", "Haldia", "Khejuri-I", "Khejuri-II", "Kolaghat", "Mahishadal", "Moyna", "Nandakumar", "Nandigram-I", "Nandigram-II", "Panskura-I", "Patashpur-I", "Patashpur-II", "Ramnagar-I", "Ramnagar-II", "Shahid Matangini", "Sutahata", "Tamluk","Digha"],
+    "Paschim Medinipur": ["Chandrakona-I", "Chandrakona-II", "Dantan-I", "Dantan-II", "Daspur-I", "Daspur-II", "Debra", "Garbeta-I", "Garbeta-II", "Garbeta-III", "Ghatal", "Keshiary", "Keshpur", "Kharagpur-I", "Kharagpur-II", "Midnapore", "Mohanpur", "Narayangarh", "Pingla", "Sabang", "Salbani","Garhbeta"],
+ 
+    Jhargram: ["Binpur-I","Binpur-Ii","Gopiballav Pur -Ii","Gopiballavpur-I","Jambani","Jhargram","Nayagram","Sankrail","Belpahari"],
+    Bankura: ["Bankura-I","Bankura-Ii","Barjora","Chhatna","Gangajal Ghati","Hirbandh","Indpur","Indus","Jaypur","Khatra-I","Kotulpur","Mejhia","Onda","Patrasayer","Raipur-I","Ranibundh","Saltora","Sarenga","Simlapal","Taldangra","Vishnupur", "Bishnupur", "Sonamukhi"],
+    Purulia: ["Arsha", "Bagmundi", "Balarampur", "Barabazar", "Bundwan", "Hura", "Jaipur", "Jhalda-I", "Jhalda-II", "Kashipur", "Manbazar-I", "Manbazar-II", "Neturia", "Para", "Puncha", "Purulia-I", "Purulia-II", "Raghunathpur-I", "Raghunathpur-II", "Santuri"],
+ 
+    Birbhum: ["Bolpur-Sriniketan","Dubrajpur","Illambazar","Khoyrasol","Labpur","Mayureswar-I","Mayureswar-Ii","Mohammad Bazar","Murarai-I","Murarai-Ii","Nalhati-I","Nalhati-Ii","Nanoor","Rajnagar","Rampurhat-I","Rampurhat-Ii","Sainthia","Suri-I","Suri-Ii"],
+    Murshidabad: ["Beldanga-I", "Beldanga-II", "Berhampore", "Bhagabangola-II", "Bhagawangola-I", "Bharatpur-I", "Bharatpur-II", "Burwan", "Domkal", "Farakka", "Hariharpara", "Jalangi", "Kandi", "Khargram", "Lalgola", "Murshidabad-Jiagunj", "Nabagram", "Nawda", "Raghunathganj-I", "Raghunathganj-II", "Raninagar-I", "Raninagar-II", "Sagardighi", "Shamsherganj", "Suti-I", "Suti-II"],
+    Nadia: ["Chakdah", "Chapra", "Hanskhali", "Haringhata", "Kaliganj", "Kalyani", "Karimpur-I", "Karimpur-II", "Krishnaganj", "Krishnagar-I", "Krishnagar-II", "Nabadwip", "Nakashipara", "Ranaghat-I", "Ranaghat-II", "Santipur", "Tehatta-I", "Tehatta-II"],
+ 
+    Malda: ["Malda", "Bamongola","Chanchal-I","Chanchal-Ii","English Bazar","Gazole","Habibpur","Harishchandrapur-I","Harishchandrapur-Ii","Kaliachak-I","Kaliachak-Ii","Kaliachak-Iii","Manikchak","Old Malda","Ratua-I","Ratua-Ii"],
+    "Uttar Dinajpur": ["Chopra", "Goalpokhar-II", "Goalpokhar-I", "Hemtabad", "Islampur", "Itahar", "Kaliaganj", "Karandighi", "Raiganj", "Dalkhola"],
+    "Dakshin Dinajpur": ["Balurghat","Bansihari","Gangarampur","Harirampur","Hili","Kumarganj","Kushmandi","Tapan"],
+ 
+    Darjeeling: ["Darjeeling", "Siliguri", "Kurseong", "Mirik","Darjeeling-Pulbazar","Jore Bunglow-Sukiapokhri","Kharibari","Matigara","Naxal Bari","Phansidewa","Rangli Rangliot"],
+    Jalpaiguri: ["Banarhat","Dhupguri","Jalpaiguri","Kranti","Mal","Matiali","Maynaguri","Nagrakata","Rajganj","Malbazar"],
+    Alipurduar: ["Alipurduar-I","Alipurduar-Ii","Falakata","Kalchini","Kumargram","Madarihat", "Jaigaon", "Hasimara"],
+    "Cooch Behar": ["Cooch Behar II","Coochbehar I","Dinhata-I","Dinhata-Ii","Haldibari","Mathabhanga II","Mathabhanga-I","Mekliganj","Sitai","Sitalkuchi","Tufanganj-I","Tufanganj-Ii"],
+    Kalimpong: ["Gorubathan","Kalimpong-I","Lava","Pedong"],
   },
 };
+ 
